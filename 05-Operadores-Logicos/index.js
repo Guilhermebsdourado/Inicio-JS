564905d587f3d1a75a66e0ec2f2ce10339bc9033
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-07-14 18:01:53 -0300

    Operadores-Logicos

---
+++ 05-Operadores-Logicos/index.js
@@ -0,0 +1,24 @@
+/*  
+    == ---- igual ---- A==B ---- verdadeiro se A for igual a B
+    OBS: esse aqui ele não ve a tipagem dele
+
+    === ---- idêntico --- A===B ---- verdadeiro se A for idêndico a B
+    OBS: esse aqui ele ve a tipagem dele, caso ele ta nas aspas ou não.
+    
+    != ---- diferente ---- A != B ---- verdadeiro se o A for diferente do A
+
+    !== ----não idêntico ----A !== B ---- verdadeiro se o A não for idêntico a B
+     OBS: esse aqui ele ve a tipagem dele, caso ele ta nas aspas ou não.
+
+    < ---- menor que ---- A < B ---- verdadeiro quando for menor que B
+
+    <= ---- menor igual que ---- A <= B ---- verdadeiro quando A for menor ou igual a B 
+
+    > ---- maior que ---- A > B verdadeiro quando o A for maior que B
+
+    >= ---- maior igual que --- A >= B verdadeiro quando for maior ou igual a B
+*/ 
+const A = 3
+const B = '3'
+
+console.log(A!==B)
