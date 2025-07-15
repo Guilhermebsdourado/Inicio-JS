7bc0384234758d5f425ef4bf5eba3d137402380f
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-07-15 19:08:26 -0300

    09-Condicionais if e else

---
+++ 09-Condicionais/index.js
@@ -0,0 +1,15 @@
+let nomeFilme = 'Caputão América'
+
+if(nomeFilme === 'Vingadores'){
+    console.log('É o filme dos Vingadores')
+}else if(nomeFilme === 'Batiman vs Superman'){
+    console.log('É o filme do Batiman vs Superman')
+}else if(nomeFilme === 'Capitão América'){
+    console.log('É o filme do Capitão América')
+}else{
+    console.log('É outro filme')
+}
+
+false ? console.log('condição verdadeira') : console.log('condição falsa')
+
+nomeFilme === 'Vingadores' ? console.log('É o filme dos Vingadores') : console.log('É outro filme')
