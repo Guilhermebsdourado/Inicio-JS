cb286dd81a7dcd256a80ccafca1609fe476a55ba
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-07-15 00:16:43 -0300

    05-Operadores-Logicos

--- 05-Operadores-Logicos/index.js
+++ 05-Operadores-Logicos/index.js
@@ -18,7 +18,50 @@
 
     >= ---- maior igual que --- A >= B verdadeiro quando for maior ou igual a B
 */ 
-const A = 3
-const B = '3'
+// const A = 3
+// const B = 3
 
-console.log(A!==B)
+// console.log(A >= B)
+
+// const a = 2
+// const b = 2
+
+console.log(a === b && a <= b)
+//V e V = V
+
+console.log(a === b && a < b)
+//V e F = F
+
+console.log(a > b && a === b)
+//F e V = F
+
+console.log(a > b && a < b)
+//F e F = F
+
+/*
+or
+*/
+
+// const a = 2
+// const b = 2
+
+console.log(a === b || a <= b)
+// V ou V = V
+
+console.log(a === b || a < b)
+//V ou F = V
+
+console.log(a > b || a === b)
+// F ou V = V
+
+console.log( a > b || a < b)
+// F ou F = F
+
+/*
+not
+*/
+
+const a = 2
+const b = 2
+
+console.log(!(a === b)) //false
