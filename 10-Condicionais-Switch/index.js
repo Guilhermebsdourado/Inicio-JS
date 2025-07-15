a340e922fd34d9c276d54547e5fd8c5e27007824
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-07-15 19:30:56 -0300

    10-Condicionais switch

---
+++ 10-Condicionais-Switch/index.js
@@ -0,0 +1,43 @@
+// switch(paramentro){
+// case valo1:
+//      <bloco de declaracão>
+//      break
+// case valor2:
+//      <bloco de declaracão>
+//      break
+// }
+
+// let nomeFilme = 'Vingadores'
+
+// switch(nomeFilme){
+//     case 'Vingadores':
+//         console.log('É o filme dos vingadores')
+//         break
+//     case 'Batman vs Superman':
+//         break
+//     case 'Senhor dos Anéis':
+//         console.log('É o filme do Senhor dos Anéis')
+//         break
+//     default:
+//         console.log('É outro filme')
+//         break
+// }
+
+let avaliacao = 3
+
+switch(avaliacao){
+    case 1:
+    case 2:
+        console.log('Filme ruim')
+        break
+    case 3:
+    case 4:
+        console.log('Filme mediano')
+        break
+    case 5:
+        console.log('Filme excelente')
+        break
+    default:
+        console.log('Nota inválida')
+        break
+}
