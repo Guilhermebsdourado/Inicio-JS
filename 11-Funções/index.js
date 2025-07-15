68fcacdc3cad20de7bb9ef9c6387e7e710a06924
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-07-15 20:02:49 -0300

    11-Funções

---
+++ 11-Funções/index.js
@@ -0,0 +1,29 @@
+
+// function nomeDaFuncao(){
+//     <bloco de execução>
+// }
+
+// nomeDaFuncao
+
+
+// function nomeDaFuncao(parametro1, parametro2){
+//     <bloco de execução>
+// }
+
+// nomeDaFuncao(parametro1, parametro2)
+
+function incentivarQuester(nomeQuester = 'Quester'){
+    alert('Muito bem,' + nomeQuester + ', você chegou no JS, continue assim!')
+}
+
+incentivarQuester()
+
+// function soma (numero1, numero2){
+//     return numero1 + numero2
+// }
+
+// let resultadoDaSoma = soma(1, 3)
+// console.log(resultadoDaSoma)//4
+
+// resultadoDaSoma = soma(resultadoDaSoma, 10)
+// console.log(resultadoDaSoma)//14
